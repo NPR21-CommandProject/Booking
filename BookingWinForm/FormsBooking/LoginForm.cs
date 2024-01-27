a70c818a6f6bcab2e87681b8db646f9c211e385f
vasiliygts <vasiliygts@gmail.com>
2024-01-27 23:14:47 +0200

    Add form of new place (variant1)

--- BookingWinForm/FormsBooking/LoginForm.cs
+++ BookingWinForm/FormsBooking/LoginForm.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace BookingWinForm
+namespace BookingWinForm.FormsBooking
 {
     public partial class LoginForm : Form
     {
