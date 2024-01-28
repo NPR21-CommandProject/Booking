f39bbbe96795901656a291e7a381028fd4ca2eb5
GucXV_yx34@student.itstep.org <GucXV_yx34@student.itstep.org>
2024-01-28 11:46:18 +0200

    Correction form

--- BookingWinForm/Migrations/20240128093851_update column Last name.Designer.cs
+++ BookingWinForm/Migrations/20240128093851_update column Last name.Designer.cs
@@ -4,6 +4,7 @@ using BookingWinForm.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookingWinForm.Migrations
 {
     [DbContext(typeof(BookingContext))]
-    partial class BookingContextModelSnapshot : ModelSnapshot
+    [Migration("20240128093851_update column Last name")]
+    partial class updatecolumnLastname
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BookingWinForm/Migrations/20240128093851_update column Last name.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingWinForm.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatecolumnLastname : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LasttName",
+                table: "tblUsers",
+                newName: "LastName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "tblUsers",
+                newName: "LasttName");
+        }
+    }
+}
