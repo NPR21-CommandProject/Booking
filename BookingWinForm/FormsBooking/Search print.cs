5dc1f108e74d14c2a71cd176e1e716dd89baa6fc
vasiliygts <vasiliygts@gmail.com>
2024-02-16 22:38:22 +0200

    Search form (Sandor M)

---
+++ BookingWinForm/FormsBooking/Search print.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookingWinForm.FormsBooking
+{
+    public partial class Search_print : Form
+    {
+        public Search_print()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BookingWinForm/FormsBooking/SearchResult.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookingWinForm.FormsBooking
+{
+    public partial class SearchResult : Form
+    {
+        public SearchResult()
+        {
+            InitializeComponent();
+        }
+
+        private void Criterii_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
