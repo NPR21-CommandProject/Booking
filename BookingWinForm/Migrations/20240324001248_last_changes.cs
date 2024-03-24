3e4b23f7cfcd30b0d783f919fe1bfc3cdc4ea781
vasiliygts <vasiliygts@gmail.com>
2024-03-24 02:17:34 +0200

    changes

---
+++ BookingWinForm/Migrations/20240324001248_last_changes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingWinForm.Migrations
+{
+    /// <inheritdoc />
+    public partial class last_changes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
